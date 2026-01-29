2e77d7cac710d909ba0715f3bc4a46bc86118900
Alexander Hurd <hurdad@gmail.com>
2026-01-28 19:15:39 -0500

    fornat

--- util/arrow.h
+++ util/arrow.h
@@ -1,49 +1,43 @@
 #pragma once
 
+#include <arrow/filesystem/filesystem.h>
+#include <arrow/filesystem/localfs.h>
+#include <arrow/result.h>
+#include <arrow/util/compression.h>
+
 #include <memory>
 #include <string>
 #include <utility>
 
-#include <arrow/result.h>
-#include <arrow/util/compression.h>
-#include <arrow/filesystem/filesystem.h>
-#include <arrow/filesystem/localfs.h>
-
 #include "arrow/arrow_common.pb.h"  // generated from your proto
 
-inline arrow::Result<
-    std::pair<std::shared_ptr<arrow::fs::FileSystem>, std::string>>
-ResolveFileSystem(const std::string& path,
-                  arrow::common::FileSystem filesystem) {
+inline arrow::Result<std::pair<std::shared_ptr<arrow::fs::FileSystem>, std::string>>
+ResolveFileSystem(const std::string& path, arrow::common::FileSystem filesystem) {
   std::string resolved_path = path;
 
   switch (filesystem) {
     case arrow::common::FILE_SYSTEM_LOCAL: {
-      return std::make_pair(std::make_shared<arrow::fs::LocalFileSystem>(),
-                            resolved_path);
+      return std::make_pair(std::make_shared<arrow::fs::LocalFileSystem>(), resolved_path);
     }
 
     case arrow::common::FILE_SYSTEM_S3:
     case arrow::common::FILE_SYSTEM_GCS:
     case arrow::common::FILE_SYSTEM_HDFS: {
-      ARROW_ASSIGN_OR_RAISE(
-          auto fs, arrow::fs::FileSystemFromUri(resolved_path, &resolved_path));
+      ARROW_ASSIGN_OR_RAISE(auto fs, arrow::fs::FileSystemFromUri(resolved_path, &resolved_path));
       return std::make_pair(std::move(fs), resolved_path);
     }
 
     case arrow::common::FILE_SYSTEM_AUTO:
     default: {
-      ARROW_ASSIGN_OR_RAISE(
-          auto fs,
-          arrow::fs::FileSystemFromUriOrPath(resolved_path, &resolved_path));
+      ARROW_ASSIGN_OR_RAISE(auto fs,
+                            arrow::fs::FileSystemFromUriOrPath(resolved_path, &resolved_path));
       return std::make_pair(std::move(fs), resolved_path);
     }
   }
 }
 
-inline arrow::Result<arrow::Compression::type>
-ResolveCompression(const std::string& path,
-                   arrow::common::Compression compression) {
+inline arrow::Result<arrow::Compression::type> ResolveCompression(
+    const std::string& path, arrow::common::Compression compression) {
   switch (compression) {
     case arrow::common::COMPRESSION_UNCOMPRESSED:
       return arrow::Compression::UNCOMPRESSED;
